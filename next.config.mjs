77c0b1b71fa2ecd398cc65e0b2524e4da02b621a
Izabela Tavares <thelifeeducation.tlp@gmail.com>
2024-11-28 00:10:38 -0300

    11:07 from yt - added pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { dev, isServer }) => {
+      if (dev && !isServer) {
+        config.watchOptions = {
+          poll: 1000, // Check for changes every second
+          aggregateTimeout: 300, // Delay before rebuilding
+        };
+      }
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
